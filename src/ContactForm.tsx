3037684cdbbc0bd7813be8d2e5f7bc1f9b1df509
Martha Taylor <marthadigitalize@gmail.com>
2022-11-14 13:35:00 +0000

    comments and console logs removed, plus message disappears in less time

--- src/ContactForm.tsx
+++ src/ContactForm.tsx
@@ -3,9 +3,6 @@ import './App.css';
 import axios from 'axios';
 import ReCAPTCHA from "react-google-recaptcha";
 
-//create html form inputs and pass them to the form submission function to send to axios via post call
-//need state to handle form elements
-
 type contactFormState = {
   firstName: string;
   surName: string;
@@ -31,30 +28,22 @@ const ContactForm = () => {
   const [contactFormState, setcontactFormState] = useState<contactFormState>(initFormState);
   const [submitting, setSubmitting] = useState<boolean>(false);
   const [endUserMessage, setendUserMessage] = useState<submittedMessage>();
-
-  //for recaptchaR so can reset element on the page
   const [recaptchaToken, setReCaptchaToken] = useState<string>();
+
   const recaptchaKey = '6LdfKwQjAAAAALrilPQxHxetUf1YPqEhNUxR27L-';
   const recaptchaRef = useRef<any>();
  
-
   const updateRecaptchaToken = (token: string | null) => {
     setReCaptchaToken(token as string);
   };
 
   const updateFormControl = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    //need to update state of form its imutible so need to create a copy and update revelant copy thats changed
     const {id, value} = event.target;
-    //typescript happy we are accessing the key of contactFormState
     const formKey = id as keyof contactFormState;
-    //shallow copy of original state
     const updatedFormState = { ...contactFormState}
-    //formkey is id we set below inside each input
     updatedFormState[formKey] = value;
-    console.log('v',value)
     setcontactFormState(updatedFormState)
   }
-
 
   const formid = 'e0yFBXHF';
   const formSparkURLcontactForm = `https://submit-form.com/${formid}`;
@@ -65,7 +54,7 @@ const ContactForm = () => {
       await postSubmission();
       setSubmitting(false);
   }
-  //submit form process in it's own seperate function
+
   const postSubmission = async() => {
     const payload = {
       ...contactFormState,
@@ -73,7 +62,6 @@ const ContactForm = () => {
     }
     try{
       const result = await axios.post(formSparkURLcontactForm, payload)
-      console.log(result);
       setendUserMessage({
         class: "submitmessage",
         text: "Thanks, someone will be in touch shortly.",
@@ -81,26 +69,27 @@ const ContactForm = () => {
       recaptchaRef.current.reset();
       setcontactFormState(initFormState);
       setTimeout( () => setendUserMessage({ class: "",
-      text: ""}), 4000);
+      text: ""}), 2000);
     }catch(error){
-      console.log(error);
       setendUserMessage({
         class: "submitmessage",
         text: "Sorry, there was a problem. Please try again or contact support.",
       });
     }
-
   } 
+  
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="contactform">
+     
         <h1>Contact us</h1>
         {endUserMessage && (
           <div className={`${endUserMessage.class}`}>
             {endUserMessage.text}
           </div>
         )}
+
        <p></p>
+       
         <form onSubmit={submitContactForm}>
           <div>
             <label htmlFor="firstName">Enter First Name </label>
@@ -114,7 +103,9 @@ const ContactForm = () => {
             <label htmlFor="email">Enter Email </label>
             <input id="email" type="text" value={contactFormState.email} onChange={updateFormControl} placeholder='Enter Email'/>
           </div>
+
           <p></p>
+
           <ReCAPTCHA
             ref={recaptchaRef}
             sitekey={recaptchaKey}
@@ -124,8 +115,6 @@ const ContactForm = () => {
           <button disabled={submitting}>{submitting ? "submitting..." : "submit"}</button>
 
         </form>
-    
-      </header>
      
     </div>
   );
